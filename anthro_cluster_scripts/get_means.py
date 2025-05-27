865f63cac1697eb7336080cd9fd9a3e333c71aeb
MarleneSchaff <mschaf11@jh.edu>
2025-05-27 18:29:05 +0100

    working get_means script

--- anthro_cluster_scripts/get_means.py
+++ anthro_cluster_scripts/get_means.py
@@ -13,23 +13,45 @@ if __name__ == "__main__":
 	args = parser.parse_args()
 
 	with gzip.open(args.embed_in, "rt") as e_in:
-		text_json = [{"text": json.loads(line)["title"]} for line in e_in]
-		word_json = [{"word": json.loads(line)["word"]} for line in e_in]
-		score_json = [{"score": json.loads(line)["score"]} for line in e_in]
-		df = pd.DataFrame()
-		df = df.assign(text = text_json, word = word_json, score = score_json)
-		df["score"] = pd.to_numeric(df["score"], errors="coerce")
+		text_json = [json.loads(line)["title"] for line in e_in]
 
-	data_means = []
-	for noun, group in df.groupby(["word"]):
-		for title, group_two in group.groupby(["text"]):
-			data_means.append({"text": title[0], "word": noun[0], "score": group_two["score"].mean()})
-	print(df.head(20))
-	#need to persist title for Kmeans labeling so don't swap for id
+	with gzip.open(args.embed_in, "rt") as em_in:
+		word_json = [json.loads(line)["word"] for line in em_in]
 
-#	df.loc = [df.groupby(["word"]).transform("nunique") > 2)
+	with gzip.open(args.embed_in, "rt") as emb_in:
+		score_json = [json.loads(line)["score"] for line in emb_in]
 
-	for text in file:
-		with open(os.path.join(args.output_dir, df["title"]), "wt") as d_out:
-			for text in data_means["text"]:
-				d_out.write(json.dumps({"text": data_means["text"], "word": data_means["word"], "score": data_means["score"]}) +"\n")
+	df = pd.DataFrame()
+	df = df.assign(text = text_json, word = word_json, score = score_json)
+	df["score"] = pd.to_numeric(df["score"], errors="coerce")
+
+	init_data = []
+	for title, group in df.groupby(["text"]):
+		for noun, group_two in group.groupby(["word"]):
+			init_data.append({"text": title[0], "word": noun[0], "score": group_two["score"].mean()})
+	#need to persist title for Kmeans labeling so not swap for id?
+
+	holding_df = pd.DataFrame.from_records(init_data)
+	data_means = holding_df.to_dict("records")
+#	if isinstance(data_means, list):
+#		print("is list")
+#	elif isinstance(data_means, dict):
+#		print("is dict")
+#	else:
+#		print("who knows")
+
+	titles_df = pd.DataFrame(df.drop_duplicates(subset=["text"]))
+
+#	for title in titles_df["text"]:
+#		for data in data_means:
+#	for data in data_means:
+#		for title in titles_df["text"]:
+#			if data["text"] == title:
+#				with open(os.path.join(args.output_dir, title), "wt") as d_out:
+#					d_out.write(json.dumps({"word": data["word"], "score": data["score"]}) + "\n")
+	for title in titles_df["text"]:
+		with open(os.path.join(args.output_dir, title), "wt") as d_out:
+			for data in data_means:
+				if data["text"] == title:
+					d_out.write(json.dumps({"word": data["word"], "score": data["score"]}) + "\n")
+					print({"text": data["text"], "word": data["word"], "score": data["score"]})
